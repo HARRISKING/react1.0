494646dd179b961badd1103012aca058c90cfa53
HARRISKING <llc13588440410@outlook.com>
2017-07-16 13:31:17 +0800

    new kuangjia

---
+++ todo-list/src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {         
+
+  constructor(props){
+    super(props)
+    this.state = {
+      newTodo: 'test1',
+      todoList: [
+        {id:1,title:'第一个代办'}
+      ]
+    }
+  }
+  render() {
+    let todos = this.state.todoList.map((item,index)=>{
+      return <li>>{item.title}</li>
+    })
+    return (
+      <div className="App">
+        <div class="inputWrapper">
+          <input type="text" value={this.state.newTodo} />
+        </div>
+        <ol>
+          {todos}
+        </ol>
+      </div>
+      
+    )
+  }
+}
+
+export default App;
