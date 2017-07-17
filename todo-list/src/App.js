589722a797b0b82508478b2573c98e5e4fdba60b
HARRISKING <llc13588440410@outlook.com>
2017-07-17 08:39:56 +0800

    new react-test

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class App extends Component {         
-
+class App extends Component { 
   constructor(props){
     super(props)
     this.state = {
